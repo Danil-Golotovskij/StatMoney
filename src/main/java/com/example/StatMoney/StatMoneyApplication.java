26236478bab06d95649dd5a69c7ba82d1950f4da
Maksim <nersus1337@mail.ru>
2024-07-05 23:15:25 +0700

    Начал выполнение задачи

--- src/main/java/com/example/StatMoney/StatMoneyApplication.java
+++ src/main/java/com/example/StatMoney/StatMoneyApplication.java
@@ -26,6 +26,6 @@ public class StatMoneyApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        System.out.println(moexService.getAllSecurities("shares"));
     }
 }
