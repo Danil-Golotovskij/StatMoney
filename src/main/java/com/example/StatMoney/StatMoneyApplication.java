1f2740e053dbbb1e700e90fe2ff06ff59b15b152
delokoseni <107359196+delokoseni@users.noreply.github.com>
2024-07-05 23:55:43 +0700

    Перед слиянием

--- src/main/java/com/example/StatMoney/StatMoneyApplication.java
+++ src/main/java/com/example/StatMoney/StatMoneyApplication.java
@@ -1,9 +1,5 @@
 package com.example.StatMoney;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import com.example.StatMoney.service.CbrService;
-import com.example.StatMoney.service.MoexService;
-import com.example.StatMoney.service.CryptoCompareService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
